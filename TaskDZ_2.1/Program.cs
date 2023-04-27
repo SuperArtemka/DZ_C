3ea8db1617e6e2056df3a83bee474293e8035236
SuperArtemka <superak@bk.ru>
2023-04-27 17:59:44 +0300

    (ДЗ Семинар 2) Вывод второй цифры трехзначного числа

---
+++ TaskDZ_2.1/Program.cs
@@ -0,0 +1,7 @@
+﻿// Вывод второй цифры трехзначного числа
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное число: {number}");
+
+int digit = number / 10 % 10;
+Console.WriteLine($"Вторая цифра этого числа: {digit}");
