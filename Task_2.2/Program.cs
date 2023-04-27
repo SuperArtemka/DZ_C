f89cad3c2c608b644a4cca0036d5fab338f3c35b
SuperArtemka <superak@bk.ru>
2023-04-27 20:02:06 +0300

    (ДЗ Семинар 2)

---
+++ Task_2.2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Вывод третьего числа, если оно есть
+Console.WriteLine("Введите трехзначное число : ");
+string number = Console.ReadLine();
+int numberLenght = number.Length;
+
+if (numberLenght > 2)
+{
+    Console.WriteLine($"Третья цифра этого числа это: {number[2]}");
+}
+else
+{
+    Console.WriteLine("Это число меньше трехзначного");
+}
+
---
+++ Task_2.3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Соответствует ли введенное число выходному дню
+Console.WriteLine("Введите число от 1 до 7: ");
+int day = Convert.ToInt32(Console.ReadLine());
+
+if (day == 6 || day == 7)
+{
+    Console.WriteLine("Это выходной день!");
+}
+else if (day <= 5)
+{
+    Console.WriteLine("Это не выходной");
+}
+else
+{
+    Console.WriteLine("Введите число от 1 до 7!");
+}
