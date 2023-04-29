dd59c6393a9a5c7a43d835e93d1674514cc8e425
SuperArtemka <superak@bk.ru>
2023-04-29 10:47:15 +0300

    ДЗ_Семинар_3 Палиндром

---
+++ TaskDZ_3.1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Палиндром
+Console.WriteLine("Введите что угодно: ");
+string symbol = Console.ReadLine();
+char[] symbolArray = symbol.ToCharArray();
+Array.Reverse(symbolArray);
+string symbolRevers = new string(symbolArray);
+if(symbol == symbolRevers){
+    System.Console.WriteLine("Супер! Это палиндром!");
+}
+else{
+    System.Console.WriteLine("Это не палиндром.");
+}
