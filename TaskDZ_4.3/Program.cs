7122d2f20644575db0ac5d059102c98adcf905f4
SuperArtemka <superak@bk.ru>
2023-05-26 11:19:43 +0300

    ДЗ_Семинар_4 Массив из восьми элементов

---
+++ TaskDZ_4.3/Program.cs
@@ -0,0 +1,10 @@
+﻿// Массив из 8 элементов заполненный в случайном порядке 
+
+int[] arr = new int[8];
+Random rand = new Random();
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = rand.Next(0, 100);
+    //System.Console.Write($"{arr[i]}");
+}
+System.Console.WriteLine('['+string.Join(",", arr)+']');
