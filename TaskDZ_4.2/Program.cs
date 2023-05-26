eda27425973bc32d063119acbfbc14674b790594
SuperArtemka <superak@bk.ru>
2023-05-26 11:13:08 +0300

    ДЗ_Семинар_4 Сумма цифр в числе

---
+++ TaskDZ_4.2/Program.cs
@@ -0,0 +1,11 @@
+﻿// Сумма цифр в числе
+
+System.Console.WriteLine("Введите число: ");
+int digit = Convert.ToInt32(Console.ReadLine());
+int digitCount = 0;
+while (digit != 0)
+{
+    digitCount += digit % 10;
+    digit /= 10;
+}
+System.Console.WriteLine($"Сумма цифр числа {digit} равна {digitCount}");
