8b4a3f3f3551e629bc01056bb69568c49d676e83
SuperArtemka <superak@bk.ru>
2023-06-02 23:38:25 +0300

    Количество четных элементов массива введенных с клавиатуры

---
+++ TaskDZ_6.1/Program.cs
@@ -0,0 +1,29 @@
+﻿// Количество чисел введенных с клавиатуры, которые больше нуля
+
+
+int[] FillArrayNumber(int size)
+{
+int[] arr = new int[size];
+for (int i = 0; i < arr.Length; i++)
+{
+System.Console.Write("Введите элемент массива: ");
+arr[i] = Convert.ToInt32(Console.ReadLine());
+}
+return arr;
+}
+
+System.Console.Write("Введите размер массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] array = FillArrayNumber(length);
+System.Console.WriteLine(string.Join(", ", array));
+
+int temp = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+if(array[i]%2==0){
+    temp++;
+}
+}
+
+System.Console.WriteLine($"Количество четных чисел: {temp}");
