6b6a45302abf83d3718c67edc6f1af4f0a5da6e7
SuperArtemka <superak@bk.ru>
2023-06-04 02:29:51 +0300

    Двумерный массив размером m x n, заполненный случайными вещественными числами.

---
+++ TaskDZ_7.1/Program.cs
@@ -0,0 +1,34 @@
+﻿// Двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+void FillMatrixWhithRandomNumbers(double[,] matrix)
+{
+Random rnd = new Random();
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+matrix[i, j] = Math.Round(rnd.Next(1, 10) + rnd.NextDouble(), 2);
+}
+}
+}
+
+void PrintMatrix(double[,] matrix)
+{
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+System.Console.Write($"{matrix[i, j]} " + "\t");
+}
+System.Console.WriteLine();
+}
+}
+
+System.Console.Write("Введите количество строк массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите количество столбцов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] matrix = new double[m, n];
+
+FillMatrixWhithRandomNumbers(matrix);
+PrintMatrix(matrix);
