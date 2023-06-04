85da3d4a9332e95f5f2ad106bdee0b5e8f9f2106
SuperArtemka <superak@bk.ru>
2023-06-04 04:15:50 +0300

    Среднее арифметическое для каждого столбца массива

---
+++ TaskDZ_7.3/Program.cs
@@ -0,0 +1,47 @@
+﻿// Среднее арифметическое для каждого столбца массива
+
+void FillMatrixWhithRandomNumbers(int[,] matrix)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(0, 11);
+        }
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write($"{matrix[i, j]} " + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void Average(int[,] matrix)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            sum += matrix[i, j];
+        }
+        System.Console.WriteLine($"Среднее арифметическое столбца: {sum / matrix.GetLength(0)}");
+    }
+}
+
+System.Console.Write("Введите количество строк массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите количество столбцов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[m, n];
+FillMatrixWhithRandomNumbers(matrix);
+PrintMatrix(matrix);
+Average(matrix);
