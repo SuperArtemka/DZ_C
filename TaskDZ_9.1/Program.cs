3020a1197bab38f65400c8d755be1487b4b23ce1
SuperArtemka <superak@bk.ru>
2023-06-10 21:42:05 +0300

    Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.

---
+++ TaskDZ_9.1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+void RoadToN(int number)
+{
+    if (number == 0)
+    {
+        return;
+    }
+    System.Console.Write($"{number} ");
+    RoadToN(number - 1);
+}
+
+System.Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+RoadToN(number);
